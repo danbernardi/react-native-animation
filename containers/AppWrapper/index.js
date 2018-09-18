d247098bd0e2f5ff1bdbe6b6abfb7c5b72fc7c48
Daniel <danbernardi1@gmail.com>
2018-09-18 15:42:23 -0700

    Adds current page title to header

--- containers/AppWrapper/index.js
+++ containers/AppWrapper/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Footer from '../../components/Footer';
 import { View } from 'react-native';
-import { node } from 'prop-types';
+import { node, func } from 'prop-types';
+import { NavigationEvents } from 'react-navigation';
+import { connect } from 'react-redux';
+import { updateNavigationState } from '../../store/actions';
 
 class AppWrapper extends Component {
   render () {
-    const { children } = this.props;
+    const { children, dispatch } = this.props;
 
     return (
       <View style={ { flex: 1 } }>
+        <NavigationEvents
+          onWillFocus={ payload => dispatch(updateNavigationState(payload)) }
+        />
         { children }
         <Footer />
       </View>
@@ -17,7 +23,8 @@ class AppWrapper extends Component {
 }
 
 AppWrapper.propTypes = {
-  children: node
+  children: node,
+  dispatch: func
 };
 
-export default AppWrapper;
+export default connect()(AppWrapper);
